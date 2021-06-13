e1893299fd7e3a184e208a90c257e2b1e2ea4b0a
JoeVu <joevu98@gmail.com>
2021-06-13 10:55:14 +0700

    remove role

--- main.js
+++ main.js
@@ -7,23 +7,12 @@ const client = new Discord.Client();
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
 client.once("ready", async () => {
-  let prevPrice = 0;
   const GUILD_ID = "846392662953033788";
   const guild = await client.guilds.fetch(GUILD_ID);
-  const roleUp = guild.roles.cache.find(role => role.name === "Price Up");
-  const roleDown = guild.roles.cache.find(role => role.name === "Price Down");
-
   const eth = setInterval(() => {
     const price = async () => {
       const price = await getPrice();
       guild.me.setNickname(`${price}`);
-      if (price > prevPrice) {
-        guild.me.roles.add(roleUp)
-        guild.me.roles.remove(roleDown)
-      } else {
-        guild.me.roles.add(roleDown)
-      }
-      prevPrice = price;
     };
     price();
   }, 5000);
